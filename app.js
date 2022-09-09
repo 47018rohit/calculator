706b3f7c3a11202971c07abed0bcd78bcb54f363
Rohit <47018rohit@gmail.com>
2022-09-09 15:11:02 +0530

    working calculator , ui can be fixed later

--- app.js
+++ app.js
@@ -3,57 +3,125 @@ const displayScreen = document.querySelector('#screen')
 const inputScreen = document.querySelector('.input')
 const previousOperand = document.querySelector('.previous')
 const currentOperand = document.querySelector('.current')
-const answerScreen = document.querySelector('.answer')
-
-let current; let previous;
-let finalDis; 
-
+const resultScreen = document.querySelector('.result')
 
 // Keypad Buttons 
 const keypad = document.querySelector('.keypad')
 const keyContainer = document.createElement('div')
-
 keypad.append(keyContainer)
 keyContainer.classList.add('keycontainer')
 keyContainer.style.gridTemplateColumns = 'repeat(4 , 1fr)';
-let button;let pressed;
-const keys = ['AC','⌫','%','÷','7','8','9','×','4','5','6','-','1','2','3','+','','0','.','=']
-keys.forEach(key=>{
-    button = document.createElement('button')
-    button.innerHTML = key
-    keyContainer.insertAdjacentElement("beforeend", button)
-    button.addEventListener('click', (e)=>{
-        pressed = e.target.innerHTML;
+let pressed;
+let current = ''; let previous = ''; let lastOperator = ''; 
+let result = null ; let haveDot = false; 
 
-        if(isNaN(pressed) && pressed != '.'){
-            
-            if(pressed == '÷' || pressed == '-' || pressed == '+' || pressed == '×'){
-                previousOperand.innerText = currentOperand.innerText,
-                previous = previousOperand.innerText;
-                currentOperand.innerHTML = '';
-            }
+const keys = ['AC','CE','%','÷','7','8','9','×','4','5','6','-','1','2','3','+','','0','.','=']
+keys.forEach(key=>{
+    const button = document.createElement('button')
+    button.innerHTML = key
+    if(button.innerHTML == 'AC'){
+            button.classList.add('all-clear');
+        }else if(button.innerHTML == 'CE'){
+            button.classList.add('clear-entity')
+        }else if(button.innerHTML == '='){
+            button.classList.add('equal-to');
+        }else if(Number(button.innerHTML) || button.innerHTML == '.' || button.innerHTML == '0'){
+            button.classList.add('number-key');
+        }else{
+            button.classList.add('operator-key');
+        }
+    keyContainer.insertAdjacentElement("beforeend", button);
+   
+});
+
+const allClear = document.querySelector('.all-clear')
+const clearEntity = document.querySelector('.clear-entity')
+const equalTo = document.querySelector('.equal-to')
+const operatorKey = document.querySelectorAll('.operator-key')
+const numberKey = document.querySelectorAll('.number-key')
+ 
+numberKey.forEach(number =>{
+    number.addEventListener('click' , (e)=>{
+        if(e.target.innerHTML ==='.' && !haveDot){
+            haveDot = true;
+        }else if(e.target.innerHTML === '.' && haveDot){
+            return;
+        }
+        current += e.target.innerHTML;
+        currentOperand.innerText = current;
+        
+    })
+});
+
+operatorKey.forEach(opkey =>{
+    opkey.addEventListener('click' , (e)=>{
+        if(!current) return;
+        haveDot = false;
+        const operator = e.target.innerHTML;
+        if(current && previous && lastOperator){
             operate();
         }else{
-            currentOperand.innerText += pressed;
-            current = currentOperand.innerHTML;
+            result = parseFloat(current);
         }
-        
-    }); 
+        clearVar(operator);
+        lastOperator = operator;
+    })
 })
 
-function operate(){
-    switch(pressed){
-        case 'AC': currentOperand.innerHTML = ''
-                 ,  answerScreen.innerText = '' 
-                 , previousOperand.innerHTML='' ;
-        break
-        case '%': answerScreen.innerText = current/100;
-        break
-        case '×':
-              answerScreen.innerText = `${previous} * ${current} `;
-              finalDis = answerScreen.innerText;
-        break
-        case '=':
-            
-    }
+function clearVar(name = ''){
+    previous += current+ ' ' + name+ ' ';
+    previousOperand.innerText = previous;
+    currentOperand.innerText = '';
+    current = '';
+    resultScreen.innerText = result;
 }
+
+function operate(){
+     switch(lastOperator){
+            
+            case '%': result = parseFloat(result) % parseFloat(current);
+            break
+            case '÷':if(current === '0'){
+                    result = '×o× '
+                    }
+                    else{
+                    result = parseFloat(result) / parseFloat(current);
+                    result = result.toFixed(4)
+                    }
+            break
+            case '×':result = parseFloat(result) * parseFloat(current),
+                     result = result.toFixed(4);
+            break
+            case '-':result = parseFloat(result) - parseFloat(current),
+                    result = result.toFixed(4);
+            break
+            case '+':result = parseFloat(result) + parseFloat(current),
+                    result = result.toFixed(4);
+            break
+        }
+}
+  
+allClear.addEventListener('click' , ()=>{
+    previous = previousOperand.innerText = '';
+    current = currentOperand.innerText = '';
+    result = resultScreen.innerText = '';
+    operator = lastOperator = '';
+})
+
+clearEntity.addEventListener('click' , ()=>{
+    currentOperand.innerText = '';
+    current = '';
+})
+        
+equalTo.addEventListener('click' , ()=>{
+    if(!current || !previous) return;
+        haveDot = false;
+        operate();
+        clearVar();
+        previous = previousOperand.innerText ='';
+        currentOperand.innerText = result;
+        resultScreen.innerText = '';
+        result = '';
+    
+    
+})
